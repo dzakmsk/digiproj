b0b362fb6bb8b30bb97365ab2d0c82f7f6739516
dzakmsk <dzakomlistov@yandex.ru>
2017-05-24 13:37:14 +0300

    Update install.cmd

--- install.cmd
+++ install.cmd
@@ -1,15 +1,1 @@
-@echo off
-
-if exist %windir%\Microsoft.Net\Framework64 goto install64
-goto install32
-
-:install32
-%windir%\Microsoft.Net\Framework\v2.0.50727\InstallUtil.exe UDPProxySvc.exe
-goto end
-
-:install64
-%windir%\Microsoft.Net\Framework64\v2.0.50727\InstallUtil.exe UDPProxySvc.exe
-goto end
-
-:end
-pause
+Hello world!
